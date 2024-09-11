e7ae8436f0eba6ef745c6eda8c9e9cb35eda4c3d
Mohamed Fouad <mohamedfouadelmezaien@outlook.sa>
2024-09-11 09:45:07 +0300

    Remove Entities And Problems

--- Shared/PCFSoftware.Infrastructure/Context/ApplicationDbContext.cs
+++ Shared/PCFSoftware.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,6 +1,4 @@
-﻿using Broker.Data.Entities.BasicData;
-using Broker.Data.Entities.BasicData.IntroductorySite;
-using Broker.Data.Entities.Identity;
+﻿using Broker.Data.Entities.Identity;
 using EntityFrameworkCore.EncryptColumn.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -19,46 +17,7 @@ namespace Broker.Infrustructure.Context
             : base(options)
         {
         }
-        public DbSet<FavouriteMarket> FavouriteMarket { get; set; }
-        public DbSet<OrderInfo> OrderInfos { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<ProductImg> ProductImgs { get; set; }
-        public DbSet<WholeSalerCategories> WholeSalerCategories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<WholeSalerOffer> WholeSalerOffers { get; set; }
-        public DbSet<Categories> Categories { get; set; }
-        public DbSet<LogExption> LogExption { get; set; }
-        public DbSet<RateDelget> RateDelget { get; set; }
-        public DbSet<ContactUs> ContactUs { get; set; }
-        public DbSet<DeviceId> DeviceIds { get; set; }
-        public DbSet<NotifyClient> NotifyClients { get; set; }
-        public DbSet<NotifyDelegt> NotifyDelegts { get; set; }
-        public DbSet<Setting> Settings { get; set; }
-        public DbSet<Copon> Copon { get; set; }
-        public DbSet<CoponUsed> CoponUsed { get; set; }
-        public DbSet<Advertisment> Advertisments { get; set; }
-        public DbSet<SocialMedia> SocialMedias { get; set; }
-        public DbSet<Questions> Questions { get; set; }
-        public DbSet<Favourite> Favourites { get; set; }
-        public DbSet<SmsMessage> SmsMessages { get; set; }
-        public DbSet<ConnectUser> ConnectUser { get; set; }
-        public DbSet<Payment> Payments { get; set; }
-        public DbSet<Messages> Messages { get; set; }
-        public DbSet<Orders> Orders { get; set; }
-        public DbSet<HistoryNotify> HistoryNotify { get; set; }
-
-        /// <summary>
-        /// Section IntroductorySite
-        /// </summary>
-        public DbSet<IntroSetting> IntroSettings { get; set; }
-        public DbSet<IntroContactUs> IntroContactUs { get; set; }
-        public DbSet<CustomerOpinion> CustomerOpinions { get; set; }
-        public DbSet<AppImg> AppImgs { get; set; }
-        public DbSet<Advantage> Advantages { get; set; }
-        public DbSet<Country> Countries { get; set; }
-        public DbSet<Governorate> Governorates { get; set; }
-        public DbSet<City> Cities { get; set; }
-        public DbSet<Regoin> Regoins { get; set; }
-
+        public DbSet<User> User { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshToken { get; set; }
     }
 }
--- Shared/Pinnacle.Data/Entities/Identity/User.cs
+++ Shared/Pinnacle.Data/Entities/Identity/User.cs
@@ -1,5 +1,4 @@
-﻿using Broker.Data.Entities.BasicData;
-using EntityFrameworkCore.EncryptColumn.Attribute;
+﻿using EntityFrameworkCore.EncryptColumn.Attribute;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,85 +8,14 @@ namespace Broker.Data.Entities.Identity
     {
         public User()
         {
-            UserRefreshTokens = new HashSet<UserRefreshToken>();
-            ContactUs = new HashSet<ContactUs>();
-            NotifyClient = new HashSet<NotifyClient>();
-            NotifyDelegt = new HashSet<NotifyDelegt>();
-            DeviceIdNavigations = new HashSet<DeviceId>();
-            Sender = new HashSet<Messages>();
-            Receiver = new HashSet<Messages>();
-            ConnectUser = new HashSet<ConnectUser>();
-            HistoryNotify = new HashSet<HistoryNotify>();
-            OrdersNavigations = new HashSet<Orders>();
-            WholeSalerOffers = new HashSet<WholeSalerOffer>();
-            WholeSalerCategories = new HashSet<WholeSalerCategories>();
-            Product = new HashSet<Product>();
+            UserRefreshTokens=new HashSet<UserRefreshToken>();
         }
-        #region WholeSaler
-        public int? CategoryId { get; set; }
-        public string? TimeFrom { get; set; }
-        public string? TimeTo { get; set; }
-        public double DeliveryPrice { get; set; } = 0;
-        #endregion
         public string FullName { get; set; }
         public string? Address { get; set; }
         public string? Country { get; set; }
-        public string? Location { get; set; } = "";
-        public string? Lat { get; set; } = "";
-        public string? Lng { get; set; } = "";
-        public bool ActiveCode { get; set; } = false;
-        public bool IsActive { get; set; } = false;
-        public bool IsDeleted { get; set; } = false;
-        // public int Code { get; set; } = 1234;
-        public string? ShowPassword { get; set; }
-        public string? Lang { get; set; } = "ar";  //اللغه هتكون عند اليوزر وتكون عربى فى الاول - وتتغير بسيرفس
-        public DateTime PublishDate { get; set; } = DateTime.Now;
-        ////تم اضافته لتعامل مع السيرفس اما 
-        ////UuserName  ده هنساويه بال email
-        //public string user_Name { get; set; } //= first_name + " " + last_name;
-        public int TypeUser { get; set; } //Client = 1  //deleget = 2 //org_delget = 3//Admin = 4
-        public bool CloseNotify { get; set; } = false; //تفعيل الاشعار
-        public string? ImgProfile { get; set; } = "";
-        // for billing
-        //[Required(ErrorMessage = "من فضلك ادخل اسم الشارع")]
-        public string? Street { get; set; }
-        //[Required(ErrorMessage = "من فضلك ادخل اسم المدينة")]
-        public string? City { get; set; }
-        //[Required(ErrorMessage = "من فضلك ادخل اسم المنطقة")]
-        public string? State { get; set; }
-        //[RegularExpression("[A-Z]{2}", ErrorMessage = "ادخل كود الدولة بشكل صحيح مثال : SA")]
-        //  public string? Country { get; set; }
-        //[Required(ErrorMessage = "من فضلك ادخل الرمز البريدي")]
-        public string? PostCode { get; set; }
-        //Relation
-        [ForeignKey(nameof(CategoryId))]
-        public virtual Categories? Category { get; set; }
-        public virtual ICollection<WholeSalerOffer> WholeSalerOffers { get; set; }
-        public virtual ICollection<WholeSalerCategories> WholeSalerCategories { get; set; }
-        //ContactUs >> user  m>> o
-        public virtual ICollection<ContactUs> ContactUs { get; set; }
-        public virtual ICollection<Product> Product { get; set; }
-        //DevieId >> user  m>> o
-        [InverseProperty(nameof(DeviceId.UserNavigation))]
-        public virtual ICollection<DeviceId> DeviceIdNavigations { get; set; }
-        //notifyclient >> user  m>> o
-        public virtual ICollection<NotifyClient> NotifyClient { get; set; }
-        //notifyDelegt >> user m>> o
-        public virtual ICollection<NotifyDelegt> NotifyDelegt { get; set; }
-        [InverseProperty(nameof(RateDelget.Deleget))]
-        public virtual ICollection<RateDelget> Delget { get; set; }
-        [InverseProperty(nameof(RateDelget.ApplicationDbUser))]
-        public virtual ICollection<RateDelget> Client { get; set; }
-        public virtual ICollection<Messages> Sender { get; set; }
-        public virtual ICollection<Messages> Receiver { get; set; }
-        [InverseProperty(nameof(Orders.User))]
-        public virtual ICollection<Orders> OrdersNavigations { get; set; }
-        public virtual ICollection<ConnectUser> ConnectUser { get; set; }
-        public virtual ICollection<HistoryNotify> HistoryNotify { get; set; }
         [EncryptColumn]
         public string? Code { get; set; }
         [InverseProperty(nameof(UserRefreshToken.user))]
         public virtual ICollection<UserRefreshToken> UserRefreshTokens { get; set; }
-
     }
 }
